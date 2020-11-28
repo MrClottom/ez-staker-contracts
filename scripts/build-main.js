16420c8871ad5efb2e2cea120112d92b416bc0db
MrClottom <philippe-dumonet@protonmail.com>
2020-11-28 18:12:35 +0100

    Final commit before 1.0.0 upload

---
+++ scripts/build-main.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+;(() => {
+  const fs = require('fs')
+
+  const loadJson = (fp) => {
+    return JSON.parse(fs.readFileSync(fp, { encoding: 'utf8', flag: 'r' }))
+  }
+
+  const targetContracts = ['IHex', 'IFeeRecipient', 'HexTransferable']
+
+  targetContracts.forEach((target) => {
+    const { contractName, abi } = loadJson(`./build/contracts/${target}.json`)
+
+    fs.writeFileSync(
+      `./abis/${target}.json`,
+      JSON.stringify({
+        contractName,
+        abi
+      })
+    )
+  })
+})()
