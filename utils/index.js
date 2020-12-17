61ba901b41083ab307d141e8d46bedabe055662a
MrClottom <philippe@dumo.net>
2020-12-17 22:52:04 +0100

    Deploy test contracts to rinkeby and prepare 1.6.0

--- utils/index.js
+++ utils/index.js
@@ -6,6 +6,10 @@ module.exports = {
     ropsten: {
       hexToken: '0x39985fe8a94351CBf838A2022b181483B43989D9',
       ezStaker: '0xE777B579cb82c65C2ed76480a35199c98F3C0901'
+    },
+    rinkeby: {
+      hexToken: '0x046D90F1614C3732Ce04D866bc9Ef0ae1Cdda509',
+      ezStaker: '0x37D1EbC3Af809b8fADB45DCE7077eFc629b2B5BB'
     }
   }
 }
